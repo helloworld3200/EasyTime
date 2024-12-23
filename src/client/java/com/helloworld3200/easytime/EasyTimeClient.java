dc5c5c61ffb890b3f1f8f62d293f60ecae29b1d4
helloworld3200 <helloworld32.tech@gmail.com>
2024-12-23 19:48:01 +0800

    initial commit

---
+++ src/client/java/com/helloworld3200/easytime/EasyTimeClient.java
@@ -0,0 +1,10 @@
+package com.helloworld3200.easytime;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EasyTimeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
